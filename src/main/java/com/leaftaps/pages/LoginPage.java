1df55185fbf4309ca7dbd55911f9ec4b90e37d69
Mathanraja <mathanrajam2m@gmail.com>
2023-09-02 12:41:42 +0530

    Line changed 13 20 27

--- src/main/java/com/leaftaps/pages/LoginPage.java
+++ src/main/java/com/leaftaps/pages/LoginPage.java
@@ -10,21 +10,21 @@ public class LoginPage extends ProjectHooks{
 	@Given("Enter the username as {string}")
 	public LoginPage enterUsername(String data) {
 		clearAndType(locateElement(Locators.ID, "username"), data);
-		reportStep(data+" entered successfully","pass");
+		reportStep(data+" Element entered successfully","pass");
 		return this;
 	}
 	
 	@And("Enter the password as {string}")
 	public LoginPage enterPassword(String data) {
 		clearAndType(locateElement(Locators.ID, "password"), data);
-		reportStep(data+" entered successfully","pass");
+		reportStep(data+" Page entered successfully","pass");
 		return this;
 	}
 	 
 	@And("Click Login button")
 	public HomePage clickLogin() {
 		click(locateElement(Locators.CLASS_NAME, "decorativeSubmit"));
-		reportStep("Login button clicked successfully", "pass");
+		reportStep("User Login button clicked successfully", "pass");
 		return new HomePage();
 	}
 
